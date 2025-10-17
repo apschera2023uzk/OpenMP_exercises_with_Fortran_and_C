c3584ba0e6501087756972f1c3da7c1a21e9d93b
Alexander Pschera <alexander.pschera@posteo.de>
2025-10-17 13:58:20 +0200

    Added Matrix Multiplikation to FROTRAN; C-Explanations are missing in README.

--- FORTRAN/test_open_mp.f90
+++ FORTRAN/test_open_mp.f90
@@ -10,18 +10,37 @@ IMPLICIT NONE
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Initializing variables:
 
-INTEGER :: i, sum
+INTEGER :: i,j,k,m, sum
+REAL :: msum
+INTEGER, DIMENSION(20, 30) :: A
+INTEGER, DIMENSION(30, 20) :: B
+INTEGER, DIMENSION(20, 20) :: C
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Executable Code:
 
 !$OMP PARALLEL
-DO i=1,100000
-    sum = sum+i
-END DO
+    DO i=1,100000
+        sum = sum+i
+    END DO
+    
+    ! C = A*B ( Es gibt theoretisch schnellere Multiplikationsalgortihmen,
+    ! aber dieser ist nach wie vor in Benutzung)
+    DO j=1,20        
+        DO k=1,20
+            msum=0.
+            DO m=1,30
+                msum = msum +A(j,m)*B(m,j)
+            END DO
+            C(j,k)=msum 
+        END DO        
+    END DO
 !$OMP END PARALLEL
 
 WRITE(*,*) "sum is:", sum
+WRITE(*,*) "Matrix A: ", A
+WRITE(*,*) "Matrix B: ", B
+WRITE(*,*) "Matrix C is: ", C
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
